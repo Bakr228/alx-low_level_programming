b1b723edb161986761d5dd88eefa8ba8d60a3d5c
bakr228 <bakrchg@gmail.com>
2023-08-24 19:49:17 -0700

    task 4

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -5,15 +5,15 @@
  * release_linked_list - deallocates a linked list
  * @list_head: list_t list to be deallocated
  */
-void release_linked_list(list_t *list_head)
+void free_list(list_t *head)
 {
-    list_t *temp;
+	list_t *temp;
 
-    while (list_head)
-    {
-        temp = list_head->next;
-        free(list_head->str);
-        free(list_head);
-        list_head = temp;
-    }
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
 }
